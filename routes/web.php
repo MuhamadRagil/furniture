a6b9bae8b079985817c1cf811bc39e3e974f7d15
MuhamadRagil <muhammadragil1403@gmail.com>
2022-08-25 23:05:25 +0700

    [ADMIN] feat(transaction): Add features transaction in admin area

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ProductGalleryController;
 
 /*
@@ -29,6 +30,9 @@ Route::middleware(['auth:sanctum' , 'verified'])->name('dashboard.')->prefix('da
         Route::resource('product', ProductController::class);
         Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only([
             'index', 'create' ,'store','destroy'
+        ]);
+        Route::resource('transaction', TransactionController::class)->only([
+            'index', 'show' ,'edit','update'
         ]);
     });
 });
